c3b38a7db29c267c5f7c6a507bba245752091e02
Manish kumar <manishmandal9734@gmail.com>
2024-11-30 11:09:34 +0530

    update back

--- backend/server/build.sh
+++
@@ -1,6 +0,0 @@
-# Adjust paths to point to backend/server
-pip install --upgrade pip
-pip install -r backend/server/requirements.txt
-python backend/server/manage.py migrate --noinput
-python backend/server/manage.py collectstatic --noinput
-python backend/server/manage.py runserver 0.0.0.0:8000
